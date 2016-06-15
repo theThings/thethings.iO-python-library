accb68a2d472742c6c9a18d43ef458a74a105585
Carles Garriga Estradé <carles.garriga@est.fib.upc.edu>
2016-06-15 11:47:44 +0200

    Changed class instance

--- examples/write_example.py
+++ examples/write_example.py
@@ -1,6 +1,6 @@
 from thethingsAPI import ThethingsAPI
 
-thethings = ThethingsAPI("ICGTa-aaHMDC2Q0SBfVcmL9gxZoChjM_JGnnFxVNjW8")
+thethings = ThethingsAPI("yourThingToken")
 thethings.addVar("hola", 1, );
 thethings.write();
 
