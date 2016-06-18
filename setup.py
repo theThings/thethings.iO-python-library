e8e36cc521398cc5274e046e153f145dcac8e11a
David Gwartney <davidg@boundary.com>
2016-06-17 18:39:27 -0700

    Add setup.py to make API easier to use

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name='thethings',
+    version='0.1.1',
+    url="https://github.com/theThings/thethings.iO-python-library",
+    packages=['thethings', ],
+    description='Python API http://thethings.io',
+)
---
+++ thethings/__init__.py
@@ -0,0 +1,1 @@
+from thethings.thethingsAPI import ThethingsAPI
